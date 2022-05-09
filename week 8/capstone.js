de8d7f20d196f5a0d9e6c2d51e31774f26ad1628
mattm988 <98669734+mattm988@users.noreply.github.com>
2022-05-09 02:15:43 -0400

    Create capstone.js

---
+++ week 8/capstone.js
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Audi Official</title>
+    <link rel="stylesheet" href="capstone.css">
+    <link href="https://fonts.googleapis.com/css2?family=Koulen&family=Roboto:ital@0;1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Koulen&family=Mukta:wght@300&family=Roboto:ital@0;1&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+       <center><a> <img src="https://logos-world.net/wp-content/uploads/2021/03/Audi-Symbol.png" width = "200px"></a></center>
+    </header>
+   <center> <nav class="nav">
+        <a href = capstone.html>Home</a>
+        <a href = about.html>About</a>
+        <a href = contact.html>Contact</a>
+    </nav>
+    </center>
+    <div class="container">
+        <img src="https://images.unsplash.com/photo-1606152421802-db97b9c7a11b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80" alt="Snow" style="width:100%;"> 
+        <div class="top-left">Confidence has no rivals.</div>
+      </div>
+      <div><center>
+          <h1>The All New 2022 RS7 Sportback.</h1>
+      </center></div>
+      <div>
+        <center><a> <img src="https://images.dealer.com/ddc/vehicles/2021/Audi/RS%207/Hatchback/perspective/front-left/2021_24.png" width = "400px"></a></center>
+      </div>
+      <div> <center><table class="table"></center>
+        <tr>
+          <th>Horsepower</th>
+          <th>Torque</th>
+          <th>0-60mph</th>
+        </tr>
+        <tr>
+          <td>591hp</td>
+          <td>590lb-ft</td>
+          <td>3.3 sec</td></div>
+        
+</body>
+</html>
